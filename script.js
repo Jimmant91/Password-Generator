98dee360abaaf9dab29cd21e1cc9ba0de1796023
James Antley <jimmy.antley@gmail.com>
2021-04-16 00:23:27 -0400

    created partial code (I'm stuck)

--- script.js
+++ script.js
@@ -13,28 +13,52 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Password Content as an Object with three keys //
+// Password content as an Object with three Keys //
 passwordCriteria = {
   specialChars: ['"', '!', '#', '%', '$', '%', '&', '(', ')', '*', '+', "'", ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '+', '`', '{' , '}', '|', '~' ],
   letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 };
 
-// Variables within the function that can be renamed depending on placement within function //
+// Uppercase Letters //
+var upperLetters = passwordCriteria.letters.map(letters => letters.toUpperCase());
+
+// User input variables //
+var confNum;
+var confChar;
+var confUpper;
+var confLower;
+var selections; // Used for concatenating key values for password(s) //
+
+// Variables within the function that can be redefined depending on placement within function //
 var create;
 
 // Defining function: generatePassword //
 function generatePassword () {
-  var create = Number(prompt("Please provide a number between 8 and 128 for the number of characters you would like your password to be."))
-};
-  function decision () {
-    if (create < 8 || create > 128) {
-      create = Number(prompt("You must choose a number between 8 and 128"))
-    }
-    else if (!create) {
-      alert("You must create a password")
-    }
-    else (create >= 8 || create <= 128) {
-      
-    }
+  create = parseInt(prompt("Please provide a number between 8 and 128 for the number of characters you would like your password to be"))
+  if (!create) {
+    alert("You must create a password");
+    generatePassword ();
   }
+  else if (create < 8 || create > 128) {
+    create = parseInt(prompt("You must choose a number between 8 and 128"));
+    generatePassword ();
+  }
+  // Else statement for confirm prompts to select password criteria for user //
+  else {
+    confNum = confirm("Would you like to include numbers?"),
+    confUpper = confirm("Would you like to incude uppercase letters?"),
+    confLower = confirm("Would you like to include lowercase leters?"),
+    confChar = confirm("Would you like to include special characters?")
+  };
+
+  // Starting with all negative selections first as this can only have one outcome //
+  if (!confNum && !confUpper && !confLower && !confChar) {
+    selection = alert("You must choose at least one critera");
+    generatePassword ();
+  }
+  else if (confNum && confUpper && confLower && confChar) {
+    selections = passwordCritera.specialChars.join() + passwordCriteria.letters.join() + passwordCriteria.numbers.join() + upperLetters.join()
+    console.log(selection)
+  }
+}
